d708d05e2b951b9a89e9a9b0460e5765d88df1d5
Ada 2018 <ada2018@172-16-2-182.0000.ifce.br.iosda.org>
2018-04-18 11:12:24 -0300

    Adição de pessoas e ajustes nos prédios

---
+++ Empreiteira/Pessoas/Cliente.swift
@@ -0,0 +1,13 @@
+//
+//  Cliente.swift
+//  Empreiteira
+//
+//  Created by Ada 2018 on 18/04/2018.
+//  Copyright © 2018 Academy. All rights reserved.
+//
+
+import Cocoa
+
+class Cliente: NSObject {
+
+}
--- Empreiteira/Predios/Predio.swift
+++ Empreiteira/Predios/Predio.swift
@@ -8,6 +8,20 @@
 
 import Cocoa
 
+enum Estado{
+    case planejamento
+    case construcao
+    case venda
+    case vendido
+}
 class Predio: NSObject {
-
+    var estado = Estado.planejamento
+    
+    // Planejamento -> Arquiteto: Elaboracao de projeto, compra do terro, etc.
+    
+    // Construcao -> Exige um terreno, engenheiro e pelo menos um mestre de obras
+    
+    // Venda -> Todo predio construido, vendedores responsaveis pela venda
+    
+    // Descreve a situação em que todas as unidades foram vendidas.
 }
