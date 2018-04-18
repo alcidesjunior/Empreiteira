6bb92ca488856cab5a7c657b8d8666eada1a9c37
Ada 2018 <ada2018@172-16-2-182.0000.ifce.br.iosda.org>
2018-04-18 09:33:04 -0300

    Adição do conjunto de profissionais.

---
+++ Empreiteira/Profissionais/Profissional.swift
@@ -0,0 +1,13 @@
+//
+//  Profissional.swift
+//  Empreiteira
+//
+//  Created by Ada 2018 on 18/04/2018.
+//  Copyright © 2018 Academy. All rights reserved.
+//
+
+import Cocoa
+
+class Profissional: NSObject {
+
+}
