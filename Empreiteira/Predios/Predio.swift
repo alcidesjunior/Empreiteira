f7da71a6e25b5ea620dc68a86ad8dcb10e23f0a5
Ada 2018 <ada2018@172-16-2-158.0000.ifce.br.iosda.org>
2018-04-18 09:32:04 -0300

    predio e ap

---
+++ Empreiteira/Predios/Predio.swift
@@ -0,0 +1,13 @@
+//
+//  Predio.swift
+//  Empreiteira
+//
+//  Created by Ada 2018 on 18/04/18.
+//  Copyright © 2018 Academy. All rights reserved.
+//
+
+import Cocoa
+
+class Predio: NSObject {
+
+}
